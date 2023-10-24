711f4351e979ffa5c57a57a39f5f358b2ab4b4e5
devops-myrepos <145119396+devops-myrepos@users.noreply.github.com>
2023-10-24 23:14:48 +0530

    Update c3-locals.tf

--- terraform-manifests/c3-locals.tf
+++ terraform-manifests/c3-locals.tf
@@ -2,8 +2,8 @@
 locals {
   owners = var.business_divsion
   environment = var.environment
-  resource_name_prefix = "${var.business_divsion}-${var.environment}"
-  #name = "${local.owners}-${local.environment}"
+  #resource_name_prefix = "${var.business_divsion}-${var.environment}"
+  resource_name_prefix = "${var.resource_group_location}-${var.business_divsion}-${var.environment}"
   common_tags = {
     owners = local.owners
     environment = local.environment
